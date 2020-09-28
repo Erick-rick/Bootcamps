b416cf98d8ec5e1de7b7713dc6765b2b3e4722f8
Erick-rick <42187451+Erick-rick@users.noreply.github.com>
2020-09-28 16:52:08 -0300

    netflix

---
+++ Projetos/Netflix/js/owl/main.js
@@ -0,0 +1,16 @@
+$ ( '.owl-carrossel' ) . owlCarousel ( {
+    loop : verdadeiro ,
+    margem : 10 ,
+    nav : true ,
+    responsivo : {
+        0 : {
+            itens : 1
+        } ,
+        600 : {
+            itens : 3
+        } ,
+        1000 : {
+            itens : 5
+        }
+    }
+} )
