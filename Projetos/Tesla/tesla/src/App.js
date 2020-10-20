5d043645ec5456cdd1fbc8f6a336f2f722d56200
Erick-rick <42187451+Erick-rick@users.noreply.github.com>
2020-10-20 17:39:30 -0300

    react native/ tesla

---
+++ Projetos/Tesla/tesla/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { GlobalStyles } from './styles/globalStyles';
+
+function App() {
+  return (
+    <>
+    <h1>teste</h1>
+
+    <GlobalStyles>
+
+    </GlobalStyles>
+    </>
+
+  );
+}
+
+export default App;
